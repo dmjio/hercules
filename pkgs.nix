cea201f6bbc959bea9a0187c27f999ce4879cf31
Domen Kožar <domen@dev.si>
2017-02-20 10:46:43 +0100

    [WIP] Switc to LTS 8.1

--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball https://github.com/NixOS/nixpkgs/archive/01ca916411fca7947be55f549ea644c9d0688e9a.tar.gz)
+import (fetchTarball https://github.com/NixOS/nixpkgs/archive/22750b36fdd96f0b88a8048e00e6ef4c31106f81.tar.gz)
